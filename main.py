38924190fc766b3cb6d7d87b85bdeb3a68cb6be4
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-07-27 17:15:47 +0200

    feat: login to instagran

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
+import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,6 +10,20 @@ CHROME_DRIVER_PATH = os.getenv("CHROME_DRIVER_PATH")
 SIMILAR_ACCOUNT = os.getenv("SIMILAR_ACCOUNT")
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
+
+
+def login(self):
+    self.driver.get("https://www.instagram.com/accounts/login/")
+    time.sleep(5)
+
+    username = self.driver.find_element_by_name("username")
+    password = self.driver.find_element_by_name("password")
+
+    username.send_keys(USERNAME)
+    password.send_keys(PASSWORD)
+
+    time.sleep(2)
+    password.send_keys(Keys.ENTER)
 
 
 class InstaFollower:
