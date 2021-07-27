89948ebe49715ef7b781acdd79b6e79a5f640e89
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-07-27 17:25:30 +0200

    feat: follow all the followers

--- main.py
+++ main.py
@@ -51,13 +51,47 @@ class InstaFollower:
         self.driver = webdriver.Chrome(executable_path=path)
 
     def login(self):
-        pass
+        self.driver.get("https://www.instagram.com/accounts/login/")
+        time.sleep(5)
+
+        username = self.driver.find_element_by_name("username")
+        password = self.driver.find_element_by_name("password")
+
+        username.send_keys(USERNAME)
+        password.send_keys(PASSWORD)
+
+        time.sleep(2)
+        password.send_keys(Keys.ENTER)
 
     def find_followers(self):
-        pass
+        time.sleep(5)
+        self.driver.get(f"https://www.instagram.com/{SIMILAR_ACCOUNT}")
+
+        time.sleep(2)
+        followers = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a'
+        )
+        followers.click()
+
+        time.sleep(2)
+        # modal = self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]')
+        modal = self.driver.find_element_by_css_selector(".isgrP")
+        for i in range(10):
+            # In this case we're executing some Javascript, that's what the execute_script() method does.
+            # The method can accept the script as well as a HTML element.
+            # The modal in this case, becomes the arguments[0] in the script.
+            # Then we're using Javascript to say: "scroll the top of the modal (popup) element by the height of the modal (popup)"
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
+            time.sleep(2)
 
     def follow(self):
-        pass
+        all_buttons = self.driver.find_elements_by_css_selector("li button")
+        for button in all_buttons:
+            if button.text != "Follow":
+                pass
+            else:
+                button.click()
+                time.sleep(2)
 
 
 bot = InstaFollower(CHROME_DRIVER_PATH)
